13046f84fe010cd0465a44ccd3081324289126e0
Kayne Middleton <kayne@anytech.ca>
2026-09-14 17:40:30 -0700

    PHP 8.5 Fixes

--- src/Services/WebPushService.php
+++ src/Services/WebPushService.php
@@ -388,7 +388,6 @@ class WebPushService
         $response = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $error = curl_error($ch);
-        curl_close($ch);
 
         if ($error) {
             $this->log('cURL error', ['error' => $error]);
